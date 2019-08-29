33e361595003b858d835799538ed2819a3ae9d4e
kosalanuwan <kosala.nuwan@gmail.com>
2019-08-29 07:56:08 +0530

    #9 fix: ng-opt-out configuration.

--- plugins/ng-optout-browser.plugin.js
+++ plugins/ng-optout-browser.plugin.js
@@ -3,7 +3,7 @@ NgOptoutBrowserPlugin.prototype.onPrepare = function () {
     const { browser } = require('protractor');
     browser.ignoreSynchronization = true;
     browser.waitForAngularEnabled(false);
-    browser.manage().window().maximize();
+    browser.driver.manage().window().maximize();
 };
 
 exports = module.exports = new NgOptoutBrowserPlugin();
